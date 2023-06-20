18da04138f0a5183aeebff8942010c5f5596ffd9
Egbo Nelson <nelsonmakuochukwu515@gmail.com>
2023-06-19 22:01:24 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Enter Point
+ * Desciption
+ * Return: Always (0)
+ */
+int main(void)
+{ printf("size of char: %lu byte(s)\n" sizeof(char);
+printf("size of int: %lu byte(s)\n" sizeof(int);
+}
