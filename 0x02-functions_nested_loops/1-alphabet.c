edcd3af799577c211bdf41cd3336703972d70e21
Egbo Nelson <nelsonmakuochukwu515@gmail.com>
2023-06-21 06:08:28 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
  * main - Entry Point
- * Description: _putchar
+ * Description: print_alphabet
  * Return: Always (0) success
  */
 void print_alphabet(void)
 {	
-int k;
-for (k = 'a'; k <= 'z'; k++)
-{	_putchar(k);
-}	
-	_putchar('\n');
-	
+	int k;
+
+	for (k = 'a'; k <= 'z'; k++)
+        {
+		_putchar(k);
+	}	
+	_putchar('\n');	
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int i;
 	int x;
 
-	for (x = 1; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{	
 		for (x = 92; x <= 122; x++)
 		{	
