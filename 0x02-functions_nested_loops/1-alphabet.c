0900d40e3dbfef764976ce779c59d34a6b70a690
Egbo Nelson <nelsonmakuochukwu515@gmail.com>
2023-06-20 18:47:06 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry Point
+ * Description: _putchar
+ * Return: Always (0) success
+ */
+void print_alphabet(void)
+{	
+	int k;
+	for (k = 97; k <= 122; k++)
+{	
+		putchar('k');
+}	
+	putchar('\n');
+}
